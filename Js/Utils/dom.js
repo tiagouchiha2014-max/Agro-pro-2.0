9d8b8db3b2241bc65ed3275cc49cefda71fae3c2
tiagouchiha2014-max <tiagouchiha2014@gmail.com>
2026-02-23 20:48:54 -0300

    Create dom.js

---
+++ Js/Utils/dom.js
@@ -0,0 +1,18 @@
+<meta name='viewport' content='width=device-width, initial-scale=1'/><script>export const $ = (sel, root = document) => root.querySelector(sel);
+export const $$ = (sel, root = document) => Array.from(root.querySelectorAll(sel));
+
+export function escapeHtml(str = "") {
+  return String(str)
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;")
+    .replaceAll("'", "&#039;");
+}
+
+export const esc = escapeHtml;
+
+export function on(el, event, handler, opts) {
+  el.addEventListener(event, handler, opts);
+  return () => el.removeEventListener(event, handler, opts);
+}</script>
